3df6aa7df3b6d8f8842d174ca86fe5b8bae4e049
Touch and Pay <rolandawoda@gmail.com>
2022-10-18 18:32:25 +0100

    feat:setup rating interface

---
+++ src/modules/rating/rating.interface.ts
@@ -0,0 +1,11 @@
+import { Document, Model } from "mongoose";
+import {IReservation} from "modules/reservation/reservation.interface"
+import { IBike } from "modules/bike/bike.interface";
+import { IUser } from "modules/user/user.interface";
+export interface IRating extends Document{
+    bike_id: IBike,
+    user_id: IUser,
+    reservation_id: IReservation,
+    rate: number,
+
+}
